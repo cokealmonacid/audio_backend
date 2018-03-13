ca806d0f7ad7fac44db418c4abcc62e851259a95
cokealmonacid <jorgealmonacid24@gmail.com>
2018-03-12 21:42:11 -0300

    Agregada ruta para crear usuarios

--- routes/v1.js
+++ routes/v1.js
@@ -4,10 +4,14 @@ const router 			= express.Router();
 const passport      	= require('passport');
 const path              = require('path');
 
+const UserController 	= require('./../controllers/UserController');
+
 require('./../middleware/passport')(passport)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({status:"success", message:"Parcel Pending API", data:{"version_number":"v1.0.0"}})
 });
+
+router.post(    '/users',           UserController.create);   
 
 module.exports = router;
