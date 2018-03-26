4b3eb3b1dae0c2a447b9640941e17b45006c9a7c
cokealmonacid <jorgealmonacid24@gmail.com>
2018-03-26 19:33:15 -0300

    Added results function to custom middleware

--- middleware/custom.js
+++ middleware/custom.js
@@ -29,3 +29,22 @@ let failures = async function (req, res, next) {
 	next();
 } 
 module.exports.failures = failures;
+
+let results = async function (req, res, next) {
+	let failure_id, audio_id, failure, audio, err;
+	failure_id = req.params.failure_id;
+	audio_id   = req.params.audio_id;
+
+	[err, failure] = await to(Failure.findOne({where:{id: failure_id}}));
+	if (err) return ReS(res, "No se ha encontrado la grabación de falla de audio");
+	if (!failure) return ReE(res, "Falla no encontrado con id: "+failure_id);
+
+	[err, audio] = await to(Audio.findOne({where:{id: audio_id}}));
+	if (err) return ReS(res, "No se ha encontrado la grabación de audio");
+	if (!audio) return ReE(res, "Audio no encontrado con id: "+audio_id);
+
+	req.failure = failure.content;
+	req.audio   = audio.content;
+	next();
+}
+module.exports.results = results;
