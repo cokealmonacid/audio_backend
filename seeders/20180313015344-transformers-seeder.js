f5484485fc8e6f7c08d2f63e57ee0fdf532f8292
cokealmonacid <jorgealmonacid24@gmail.com>
2018-04-15 18:38:17 -0300

    Added createdAt and updatedAt to transformers seeder

--- seeders/20180313015344-transformers-seeder.js
+++ seeders/20180313015344-transformers-seeder.js
@@ -6,32 +6,44 @@ module.exports = {
       {
         brand : 'brand_1',
         model : 'model_1',
-        year  : 1991
+        year  : 1991,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },
       {
         brand : 'brand_2',
         model : 'model_2',
-        year  : 1992
+        year  : 1992,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },
       {
         brand : 'brand_3',
         model : 'model_3',
-        year  : 1993
+        year  : 1993,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },
       {
         brand : 'brand_4',
         model : 'model_4',
-        year  : 1994
+        year  : 1994,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },
       {
         brand : 'brand_5',
         model : 'model_5',
-        year  : 1995
+        year  : 1995,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },
       {
         brand : 'brand_6',
         model : 'model_6',
-        year  : 1996
+        year  : 1996,
+        createdAt : '2018-04-15 15:19:06',
+        updatedAt : '2018-04-15 15:19:06'
       },   
     ], {});
   },
