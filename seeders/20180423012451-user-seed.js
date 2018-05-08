14a00793fa6e883670396d931fc21d2d8959d73d
cokealmonacid <jorgealmonacid24@gmail.com>
2018-05-07 21:20:05 -0300

    Added new users to seed

--- seeders/20180423012451-user-seed.js
+++ seeders/20180423012451-user-seed.js
@@ -9,6 +9,18 @@ module.exports = {
         createdAt   : '2018-04-15 15:19:06',
         updatedAt   : '2018-04-15 15:19:06'
       },
+      {
+        email       : 'a@a.com',
+        password    : '$2a$10$CYS3MmYhom0SBEhXMJHXsexeytChE1EsOeIDVRw2TxkwGjEw55ma.',
+        createdAt   : '2018-04-15 15:19:06',
+        updatedAt   : '2018-04-15 15:19:06'  
+      },
+      {
+        email       : 'b@b.com',
+        password    : '$2a$10$CYS3MmYhom0SBEhXMJHXsexeytChE1EsOeIDVRw2TxkwGjEw55ma.',
+        createdAt   : '2018-04-15 15:19:06',
+        updatedAt   : '2018-04-15 15:19:06'
+      }
     ], {});
   },
 
