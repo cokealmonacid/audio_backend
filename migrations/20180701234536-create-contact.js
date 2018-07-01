d6f5aaebb66facee3e05748be4f38bedfbbe70d9
cokealmonacid <jorgealmonacid24@gmail.com>
2018-07-01 19:50:37 -0400

    Added migration and model of Contact

---
+++ migrations/20180701234536-create-contact.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Contacts');
+  }
+};
